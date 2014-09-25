457ec706c98dea163bb38d3d2bf7c1f888e9bc6e
Virola <virola.zhu@gmail.com>
2014-09-25 16:22:26 +0800

    new

--- php/show.php
+++ php/show.php
@@ -36,12 +36,14 @@ $msg_arr = array();
 if ($result) {
     while($row = mysql_fetch_array($result)) {
         $issend = ($row['authorid'] == $targetid ? false : true);
+        $min = (int)date('i', $row['dateline']);
+        
         $msg_arr[] = array(
             'pmid' => $row['pmid'],
             'userid' => $row['authorid'],
             'username' => $row['username'],
             'content' => $row['content'],
-            'dateline' => date('Y年n月d日 G:i', $row['dateline']),
+            'dateline' => date('Y年n月d日 G:', $row['dateline']).$min,
             'issend' => $issend,
             'avator' => ($issend ? $user['avator'] : $tuser['avator']),
         );
